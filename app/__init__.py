af5580b25337b5f0fa19351026fdff2d25f09207
Khalid Sinan <iidrgames1@gmail.com>
2025-06-26 23:12:24 +0300

    Add BluePrints to App

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from .config import Config
 from .extensions import db, ma, cors
 from .routes.manager_routes import manager_bp
+from .routes.member_routes import member_bp
+from .routes.skill_routes import skill_bp
 
 
 def create_app():
@@ -15,5 +17,7 @@ def create_app():
 
     # Register blueprints
     app.register_blueprint(manager_bp, url_prefix='/api/managers')
+    app.register_blueprint(member_bp, url_prefix='/api/members')
+    app.register_blueprint(skill_bp, url_prefix='/api/skills')
 
     return app
--- run.py
+++ run.py
@@ -4,5 +4,8 @@ from app.extensions import db
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
     app.run(debug=True)
